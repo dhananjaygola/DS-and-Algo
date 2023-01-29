03da9904f76e69a55acdd4d88c2212e037629f29
dhananjaygola <dhananjay.181018@gmail.com>
2023-01-30 00:29:54 +0530

    Create PriorityQueue.cpp

---
+++ Arrays/Kth Smallest Largest Element in Unsorted Array/PriorityQueue.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int a[]={2,0,4,-7},k;
+	int size = sizeof(a)/sizeof(a[0]);
+	cout<<"Enter any number < "<<size<<" : ";
+	cin>>k;
+	priority_queue<int> q;
+	for(int i=0;i<k;++i){
+		
+	}
+}
