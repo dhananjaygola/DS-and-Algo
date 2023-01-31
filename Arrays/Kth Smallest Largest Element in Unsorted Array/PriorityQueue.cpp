be5f2059db5f6b7b80f825f80eb9eeda48ae1d67
dhananjaygola <dhananjay.181018@gmail.com>
2023-01-31 22:43:10 +0530

    Update PriorityQueue.cpp

--- Arrays/Kth Smallest Largest Element in Unsorted Array/PriorityQueue.cpp
+++ Arrays/Kth Smallest Largest Element in Unsorted Array/PriorityQueue.cpp
@@ -1,6 +1,16 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+int Ksmallest(priority_queue<int> q,int a[],int size,int k){
+	for(int i=k;i<size;++i){
+		if(q.top()>a[i]){
+			q.pop();
+			q.push(a[i]);
+		}
+	}
+	return q.top();
+}
+
 int main(){
 	int a[]={2,0,4,-7},k;
 	int size = sizeof(a)/sizeof(a[0]);
@@ -8,6 +18,7 @@ int main(){
 	cin>>k;
 	priority_queue<int> q;
 	for(int i=0;i<k;++i){
-		
+		q.push(a[i]);
 	}
+	cout<<Ksmallest(q,a,size,k);
 }
