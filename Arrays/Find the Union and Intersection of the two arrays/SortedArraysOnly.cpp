ae8ee73617cf068fac39ee535f6711c38e161a2f
dhananjaygola <dhananjay.181018@gmail.com>
2023-03-02 23:34:22 +0530

    commit

--- Arrays/Find the Union and Intersection of the two arrays/SortedArraysOnly.cpp
+++ Arrays/Find the Union and Intersection of the two arrays/SortedArraysOnly.cpp
@@ -1,29 +1,28 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 int main(){
-	int a[] = {2,4,-1,3,-9};
-	int b[] = {10,3,-1,11};
-	int i = 0, j = 0;
+	int a[] = {0,2,3,5};
+	int b[] = {5,6,7};
 	int size1 = sizeof(a)/sizeof(a[0]);
 	int size2 = sizeof(b)/sizeof(b[0]);
-	sort(a,a+size1);
-	sort(b,b+size2);
-	while(i<size1&&j<size2){
+	int i = 0, j = 0;
+	while((i<size1)&&(j<size2)){
 		if(a[i]<b[j]){
 			cout<<a[i]<<" ";
 			++i;
 		}
-		else if(a[i]>b[j]){
+		else if(b[j]<a[i]){
 			cout<<b[j]<<" ";
 			++j;
 		}
 		else{
-			cout<<b[j]<<" ";
+			cout<<a[i]<<" ";
 			++i;
 			++j;
 		}
 	}
+	
 	while(i<size1){
 		cout<<a[i]<<" ";
 		++i;
