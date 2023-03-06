a52f05fb2b7f85abd1a08ac280aa882186b863e5
dhananjaygola <dhananjay.181018@gmail.com>
2023-03-06 13:25:22 +0530

    Update KadanesAlgorithm.cpp

--- Arrays/Largest sum contiguous Subarray/KadanesAlgorithm.cpp
+++ Arrays/Largest sum contiguous Subarray/KadanesAlgorithm.cpp
@@ -5,8 +5,8 @@ int main(){
 	int a[] = {2,-1,0,-3};
 	int size = sizeof(a)/sizeof(a[0]);
 	int max_current = a[0], max_global = a[0];
-	for(int i=1;i<size;++i){
-		max_current = max(a[i], max_current + a[i]);
+	for(int i = 1; i < size; ++i){
+		max_current = max( a[i], max_current + a[i]);
 		if(max_current > max_global)
 			max_global = max_current;
 	}
