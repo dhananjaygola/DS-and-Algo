e522f624e8427740465c69199e1149f47ca19006
dhananjaygola <dhananjay.181018@gmail.com>
2023-03-11 01:21:11 +0530

    Create OptimisedMethod.cpp

---
+++ Arrays/Minimum number of jumps to reach end of an array/OptimisedMethod.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int minJumps(int a[], int size){
+	if(size <= 1)
+		return 0;
+	if(a[0] >= size-1)
+		return 1;
+	if(a[0] == 0)
+		return -1;
+		
+	int maxReach = a[0], step = a[0], jump = 1;
+	for(int i = 1; i<size; ++i){
+		if(i == size-1)
+			return jump;
+		if(a[i] >= (size-1) - i)
+			return jump+1;
+		maxReach = max(maxReach, i+a[i]);
+		step--;
+		if(step == 0){
+			++jump;
+			if(i >= maxReach)
+				return -1;
+			step = maxReach - i;
+		}
+	}
+	return -1;
+}
+
+int main(){
+	int a[] = {4,2,3,4,1,6,7};
+	int size = sizeof(a)/sizeof(a[0]);
+	cout<<minJumps(a,size);
+}
