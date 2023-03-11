e2d5331760834496b631b4e211ed413029fbd57b
dhananjaygola <dhananjay.181018@gmail.com>
2023-03-12 01:47:44 +0530

    Update ArrayHashmap.cpp

--- Arrays/Find duplicate in an array of N+1 Integers/ArrayHashmap.cpp
+++ Arrays/Find duplicate in an array of N+1 Integers/ArrayHashmap.cpp
@@ -2,15 +2,14 @@
 using namespace std;
 
 int Duplicate(int a[], int size){
-	while(1>0){
-		if(a[a[0]] == a[0])
-			return a[0];
-		swap(a[a[0]], a[0]);
-	}
+	if(a[a[0]] == a[0])
+		return a[0];
+	swap(a[a[0]], a[0]);
+	Duplicate(a,size);
 }
 
 int main(){
-	int a[] = {1,2,3,2,4};
+	int a[] = {4,2,3,1,3};
 	int size = sizeof(a)/sizeof(a[0]);
 	cout<<Duplicate(a,size);
 }
